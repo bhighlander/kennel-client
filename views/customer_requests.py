6a0a29b6464e7e4029433d666ca80152d2ef5bc9
Brandon Highland <coffeemonsterstudio@gmail.com>
2023-07-17 11:51:48 -0500

    completed chapters 1, 2 of book 2

--- views/customer_requests.py
+++ views/customer_requests.py
@@ -1,3 +1,7 @@
+import sqlite3
+import json
+from models import Customer
+
 CUSTOMERS = [
     {
         "id": 1,
@@ -14,16 +18,45 @@ CUSTOMERS = [
 ]
 
 def get_all_customers():
-    return CUSTOMERS
+    with sqlite3.connect("./kennel.sqlite3") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        SELECT
+            c.id,
+            c.name
+        FROM customer c
+        """)
+
+        customers = []
+
+        dataset = db_cursor.fetchall()
+
+        for row in dataset:
+            customer = Customer(row['id'], row['name'])
+            customers.append(customer.__dict__)
+
+    return customers
 
 def get_single_customer(id):
+    with sqlite3.connect("./kennel.sqlite3") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
 
-    requested_customer = None
-    for customer in CUSTOMERS:
-        if customer["id"] == id:
-            requested_customer = customer
+        db_cursor.execute("""
+        SELECT
+            c.id,
+            c.name
+        FROM customer c
+        WHERE c.id = ?
+        """, ( id, ))
 
-    return requested_customer
+        data = db_cursor.fetchone()
+
+        customer = Customer(data['id'], data['name'])
+
+        return customer.__dict__
 
 def create_customer(customer):
     max_id = CUSTOMERS[-1]["id"]
