3bf7c7489ca787aa5af03a19e74f54f42720d8d1
Brandon Highland <coffeemonsterstudio@gmail.com>
2023-07-18 15:40:35 -0500

    completed chapters 1-7

--- views/animal_request.py
+++ views/animal_request.py
@@ -3,6 +3,8 @@ import json
 from .customer_requests import get_single_customer
 from .location_requests import get_single_location
 from models import Animal
+from models import Location
+from models import Customer
 
 ANIMALS = [
     {
@@ -48,9 +50,19 @@ def get_all_animals():
             a.breed,
             a.status,
             a.location_id,
-            a.customer_id
-        FROM animal a
+            a.customer_id,
+            l.name location_name,
+            l.address location_address,
+            c.id customer_id,
+            c.name customer_name,
+            c.email customer_email
+        FROM Animal a
+        JOIN Location l
+            ON l.id = a.location_id
+        JOIN Customer c
+            ON c.id = a.customer_id
         """)
+
 
         # Initialize an empty list to hold all animal representations
         animals = []
@@ -59,18 +71,27 @@ def get_all_animals():
         dataset = db_cursor.fetchall()
 
         # Iterate list of data returned from database
+
         for row in dataset:
 
-            # Create an animal instance from the current row.
-            # Note that the database fields are specified in
-            # exact order of the parameters defined in the
-            # Animal class above.
-            animal = Animal(row['id'], row['name'], row['breed'],
-                            row['status'], row['location_id'],
-                            row['customer_id'])
+            # Create an animal instance from the current row
+            animal = Animal(row['id'], row['name'], row['breed'], row['status'],
+                            row['location_id'], row['customer_id'])
 
+            # Create a Location instance from the current row
+            location = Location(row['id'], row['location_name'], row['location_address'])
+
+            customer = Customer(row['customer_id'], row['customer_name'], row['customer_email'])
+
+
+            # Add the dictionary representation of the location to the animal
+            animal.location = location.__dict__
+
+            # Add the dictionary representation of the customer to the animal
+            animal.customer = customer.__dict__
+
+            # Add the dictionary representation of the animal to the list
             animals.append(animal.__dict__)
-
     return animals
 
 def get_single_animal(id):
@@ -110,16 +131,86 @@ def create_animal(animal):
     return animal
 
 def delete_animal(id):
-    animal_index = -1
-    for index, animal in enumerate(ANIMALS):
-        if animal["id"] == id:
-            animal_index = index
-    if animal_index >= 0:
-        ANIMALS.pop(animal_index)
+    with sqlite3.connect("./kennel.sqlite3") as conn:
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        DELETE FROM animal
+        WHERE id = ?
+        """, (id, ))
 
 def update_animal(id, new_animal):
-    for index, animal in enumerate(ANIMALS):
-        if animal["id"] == id:
-            new_animal["id"] = id
-            ANIMALS[index] = new_animal
-            break
+    with sqlite3.connect("./kennel.sqlite3") as conn:
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        UPDATE Animal
+            SET
+                name = ?,
+                breed = ?,
+                status = ?,
+                location_id = ?,
+                customer_id = ?
+        WHERE id = ?
+        """, (new_animal['name'], new_animal['breed'],
+            new_animal['status'], new_animal['locationId'],
+            new_animal['customerId'], id, ))
+
+        # Were any rows affected?
+        # Did the client send an `id` that exists?
+        rows_affected = db_cursor.rowcount
+
+    if rows_affected == 0:
+        # Forces 404 response by main module
+        return False
+    else:
+        # Forces 204 response by main module
+        return True
+
+def get_animals_by_location(location_id):
+    with sqlite3.connect("./kennel.sqlite3") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+        db_cursor.execute("""
+        SELECT
+            a.id,
+            a.name,
+            a.breed,
+            a.status,
+            a.location_id,
+            a.customer_id
+        FROM animal a
+        WHERE a.location_id = ?
+        """, ( location_id, ))
+        animals = []
+        dataset = db_cursor.fetchall()
+        for row in dataset:
+            animal = Animal(row['id'], row['name'], row['breed'],
+                            row['status'], row['location_id'],
+                            row['customer_id'])
+            animals.append(animal.__dict__)
+    return animals
+
+def get_animals_by_status(status):
+    with sqlite3.connect("./kennel.sqlite3") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+        db_cursor.execute("""
+        SELECT
+            a.id,
+            a.name,
+            a.breed,
+            a.status,
+            a.location_id,
+            a.customer_id
+        FROM animal a
+        WHERE a.status = ?
+        """, ( status, ))
+        animals = []
+        dataset = db_cursor.fetchall()
+        for row in dataset:
+            animal = Animal(row['id'], row['name'], row['breed'],
+                            row['status'], row['location_id'],
+                            row['customer_id'])
+            animals.append(animal.__dict__)
+    return animals
--- views/customer_requests.py
+++ views/customer_requests.py
@@ -34,7 +34,7 @@ def get_all_customers():
         dataset = db_cursor.fetchall()
 
         for row in dataset:
-            customer = Customer(row['id'], row['name'])
+            customer = Customer(row['id'], row['name'], row['email'], row['password'])
             customers.append(customer.__dict__)
 
     return customers
@@ -54,7 +54,7 @@ def get_single_customer(id):
 
         data = db_cursor.fetchone()
 
-        customer = Customer(data['id'], data['name'])
+        customer = Customer(data['id'], data['name'], data['email'], data['password'])
 
         return customer.__dict__
 
@@ -79,3 +79,27 @@ def update_customer(id, new_customer):
             new_customer["id"] = id
             CUSTOMERS[index] = new_customer
             break
+
+def get_customers_by_email(email):
+    with sqlite3.connect("./kennel.sqlite3") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        SELECT
+            c.id,
+            c.name,
+            c.email,
+            c.password
+        FROM customer c
+        WHERE c.email = ?
+        """, ( email, ))
+
+        customers = []
+        dataset = db_cursor.fetchall()
+
+        for row in dataset:
+            customer = Customer(row['id'], row['name'], row['email'], row['password'])
+            customers.append(customer.__dict__)
+
+    return customers
